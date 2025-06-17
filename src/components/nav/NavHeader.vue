fd2bccbbe56c3a54b5957176485c8dd93883b726
Nikita <nikita.vorontsov.1989@mail.ru>
2025-06-17 15:05:18 +0300

    add page payment basket and add router add nav btn

--- src/components/nav/NavHeader.vue
+++ src/components/nav/NavHeader.vue
@@ -7,5 +7,6 @@ import NavHeaderItem from "@/components/nav/NavHeaderItem.vue";
   <nav>
     <NavHeaderItem to="/home" text="Home" />
     <NavHeaderItem to="/about" text="About" />
+    <NavHeaderItem to="/basket" text="Payment Basket" />
   </nav>
 </template>
